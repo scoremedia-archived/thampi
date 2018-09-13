f07abf052a1a6bb2b946634498138f32b9348af1
Rajiv Abraham <rajiv.abraham@thescore.com>
2018-09-13 16:14:33 -0400

    more debug

--- thampi/core/flask_api.py
+++ thampi/core/flask_api.py
@@ -1,26 +1,33 @@
+print('Raj: Start of Thampi Flask API')
 from flask import Flask, request, jsonify
+print('Raj: After Flask Import')
 from thampi.core.thampi_core import Thampi
+print('Raj: After Thampi Core Import')
 import json
 
 app = Flask(__name__)
 
 thampi_instance = Thampi(app)
 
+print('Raj: Init: Before Global Model Loaded')
 global_model = thampi_instance.load_model()
-print('Raj: Init: Global Model loaded')
+print('Raj: Init: After Global Model Loaded')
 global_properties = thampi_instance.load_properties()
-print('Raj: Init: Global Properties loaded')
+print('Raj: Init: After Global Properties loaded')
 
 
 @app.route(thampi_instance.predict_route(), methods=['POST'])
 def index():
+    print('Raj: Start Index Method')
     global global_model
     args = json.loads(request.data)
     if not global_model:
         global_model = thampi_instance.load_model()
         print('Raj: Request: Global Properties loaded again')
 
+    print('Raj: Before Predict')
     result = global_model.predict(args.get('data'), thampi_instance.get_context())
+    print('Raj: After Predict')
     return jsonify(dict(properties=global_properties,
                         result=result))
 
