7a6206bca972ff6cf7312e7b3722927e7d87ef16
Rajiv Abraham <rajiv.abraham@thescore.com>
2018-09-13 14:45:47 -0400

    debug

--- thampi/core/flask_api.py
+++ thampi/core/flask_api.py
@@ -7,16 +7,21 @@ app = Flask(__name__)
 thampi_instance = Thampi(app)
 
 global_model = thampi_instance.load_model()
+print('Raj: Init: Global Model loaded')
 global_properties = thampi_instance.load_properties()
+print('Raj: Init: Global Properties loaded')
 
 
 @app.route(thampi_instance.predict_route(), methods=['POST'])
 def index():
     args = json.loads(request.data)
+    if not global_model:
+        global_model = thampi_instance.load_model()
+        print('Raj: Request: Global Properties loaded again')
+    
     result = global_model.predict(args.get('data'), thampi_instance.get_context())
     return jsonify(dict(properties=global_properties,
                         result=result))
-    # return jsonify(dict(properties=properties, prediction=prediction))
 
 
 # We only need this for local development.
